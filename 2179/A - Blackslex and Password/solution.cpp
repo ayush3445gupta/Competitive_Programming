cccbf77376c6d80e3a5e55a39f0a2fa9b6fad33a
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2025-12-23 20:29:52 +0530

    Add Blackslex and Password [A] from Codeforces

---
+++ 2179/A - Blackslex and Password/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std;typedef long long ll;void f(ll n,vector<ll>&a){    for(ll i=0;i<n;i++)cin>>a[i];}void solve(){    ll k,x;    cin>>k>>x;    cout<<k*x+1<<'\n';} int main(){ll t;cin>>t;while (t--){    solve();}     return 0;}
