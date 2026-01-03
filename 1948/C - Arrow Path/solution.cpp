980f42e851622fa784176c5033bb981c0e84c926
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2026-01-03 17:00:09 +0530

    Add Arrow Path [C] from Codeforces

---
+++ 1948/C - Arrow Path/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std;void solve(){	int n;cin>>n;	string c1,c2;	cin>>c1>>c2;	for(int i=2;i<n-1;i+=2)	if(c2[i]=='<'&&(c1[i-1]=='<'||c1[i+1]=='<')){cout<<"NO"<<endl;return;}	cout<<"YES"<<endl;}int main(){	int n;	cin>>n;	while(n--)	solve();}
