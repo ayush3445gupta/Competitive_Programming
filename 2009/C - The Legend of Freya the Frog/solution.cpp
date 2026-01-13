6e87cce5509f59af1a323b12c9b35818738d6580
ayush3445gupta <118062467+ayush3445gupta@users.noreply.github.com>
2026-01-13 12:27:39 +0530

    Add The Legend of Freya the Frog [C] from Codeforces

---
+++ 2009/C - The Legend of Freya the Frog/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std;typedef long long ll;void f(ll n,vector<ll>&a){    for(ll i=0;i<n;i++)cin>>a[i];}void solve(){    ll x,y,k;    cin>>x>>y>>k;    ll a=ceil(x/(1.0*k)),b=ceil(y/(1.0*k));    ll ans;    if(a>b)ans=2*a-1;    else ans=2*b;    cout<<ans<<'\n';} int main(){ios::sync_with_stdio(0);cin.tie(0);ll t;cin>>t;while (t--){    solve();}     return 0;}
